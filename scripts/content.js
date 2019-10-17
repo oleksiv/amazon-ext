12a26f92bbb7155c9b0d01f1378ee8ff77a43b67
sviat <hi.oleksiv@gmail.com>
2019-10-17 17:14:48 +0300

    Added check for restriction and ungate the product

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,24 @@
 'use strict';
 
-const MESSAGE_TYPE_ASIN = 1;
 
-const ASIN = window.location.href.match(/(?:[/dp/]|$)([A-Z0-9]{10})/i);
+const MESSAGE_TYPE_HAZMAT = 1;
+const MESSAGE_TYPE_UNGATE = 2;
+const MESSAGE_TYPE_RESTRICTIONS = 3;
 
-chrome.runtime.sendMessage({type: MESSAGE_TYPE_ASIN, message: ASIN[1]});
+const ASIN = window.location.href.match(/(?:[/dp/]|$)([A-Z0-9]{10})/i)[1];
+const DOMAIN = 'co.uk';
+
+chrome.runtime.sendMessage({type: MESSAGE_TYPE_HAZMAT, message: ASIN, domain: DOMAIN}, function (response) {
+    console.log(response);
+});
+
+chrome.runtime.sendMessage({type: MESSAGE_TYPE_UNGATE, message: ASIN, domain: DOMAIN}, function (response) {
+    console.log(response);
+});
+
+chrome.runtime.sendMessage({type: MESSAGE_TYPE_RESTRICTIONS, message: ASIN, domain: DOMAIN}, function (response) {
+    console.log(response);
+});
 
 console.log(ASIN[1]);
 
